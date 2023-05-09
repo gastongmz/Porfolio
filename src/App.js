c405d8ffc482c5d317c338b9c2ba73b2145500e4
Gomez Gaston Alejandro <gaston.alejandro.gmz@gmail.com>
2023-05-09 14:39:40 -0300

    pathServer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { useState } from "react";
-
 import { ThemeContext } from './contexts/ThemeContext';
-import { Main, BlogPage, ProjectPage, LoginPage,GridPage } from './pages'
+import { Main, BlogPage, ProjectPage, LoginPage, GridPage } from './pages'
 import { BackToTop } from './components'
 import ScrollToTop from './utils/ScrollToTop'
 import PrivateRoute from './utils/PrivateRoute';
-
-
-
 import './App.css'
-import Protected from './utils/Protected';
-
-
 
 function App() {
 
@@ -23,39 +16,19 @@ function App() {
   console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);
   // console.log = console.warn = console.error = () => {};
 
-  //const pathServer= '/TPO'
-  
-  /*
-  const [isLoggedIn, setisLoggedIn] = useState();
-  const logIn = () => {
-  setisLoggedIn(true);
-  localStorage.setItem("auth", JSON.stringify(true));
-  console.log("setisLoggedIn:" + isLoggedIn)
-  };
-  const logOut = () => {    
-  setisLoggedIn(false);
-  localStorage.setItem("auth", JSON.stringify(false));
-  console.log("setisLoggedIn:" + isLoggedIn)
-  };  */
-  
-  const pathServer= ''
+  const pathServer = '/TPO'
+  //const pathServer= ''
   return (
     <div className="app">
-   {/*   {isLoggedIn ? (        
-  <button onClick={logOut}>Logout</button>
-      ) : (
-  <button onClick={logIn}>Login</button>
-      )}*/}
       <Router>
-        <ScrollToTop/>
+        <ScrollToTop />
         <Switch>
-          
-          <Route path={pathServer +"/"} exact component={Main} />
-          <Route path={pathServer +"/login"} exact component={LoginPage} /> 
-          <PrivateRoute component={GridPage} path="/grid" exact/>                   
-           {/*<Protected component={GridPage} path="/grid" exact/>   */}
-          <Route path={pathServer +"/blog"} exact component={BlogPage} />
-          <Route path={pathServer +"/projects"} exact component={ProjectPage} />
+
+          <Route path={pathServer + "/"} exact component={Main} />
+          <Route path={pathServer + "/login"} exact component={LoginPage} />
+          <PrivateRoute component={GridPage} path="/grid" exact />
+          <Route path={pathServer + "/blog"} exact component={BlogPage} />
+          <Route path={pathServer + "/projects"} exact component={ProjectPage} />
 
           <Redirect to="/" />
         </Switch>
